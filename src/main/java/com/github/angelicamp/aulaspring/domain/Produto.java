a17a08a2f92a25ca5096f8bb380af663e6c1b219
angelicamp <85784665+angelicamp@users.noreply.github.com>
2022-01-11 13:32:32 -0300

    Criado Produto e produto repository associação muitos pra muitos entre produtos e categoria

---
+++ src/main/java/com/github/angelicamp/aulaspring/domain/Produto.java
@@ -0,0 +1,45 @@
+package com.github.angelicamp.aulaspring.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Produto implements Serializable {
+    private static final long serialVersionUID =1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Double produto;
+
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"),inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
